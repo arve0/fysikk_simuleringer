81b244031424c0fddf01f946bb5aa73a5f23eb0b
arve0 <arve.seljebu@gmail.com>
2016-10-16 22:20:20 +0200

    sammenligne med null luftmotstand og vis akselerasjon og fart på graf

--- kast/kast.js
+++ kast/kast.js
@@ -32,13 +32,11 @@ coordinateResize();
  * a_x = -Kv_x^2/m
  * a_y = -(g + Kv_x^2/m)
  */
-var xs, ys;
 function numericThrow () {
   ctx.restore();
   ctx.clearRect(0, 0, coordinatesystem.width, coordinatesystem.height);
   coordinateResize();
 
-  var scale = 100;
   var x_0 = parseFloat(parameters.x_0.value);
   var y_0 = parseFloat(parameters.y_0.value);
   var v_0 = parseFloat(parameters.v_0.value);
@@ -50,20 +48,42 @@ function numericThrow () {
   };
   var m = parseFloat(parameters.mass.value);
 
-  ctx.moveTo(scale * x_0, -scale * y_0);
-
-  xs = [x_0];
-  ys = [y_0];
-  var delta_t = 0.01;
   var v = {
     x: v_0 * Math.cos(angle / 180 * Math.PI),
     y: v_0 * Math.sin(angle / 180 * Math.PI)
   };
   var a = { x: 0, y: g };
 
-  var i = 0;
+  // green without drag
+  var K_no_drag = { x: 0, y: 0 };
+  draw(x_0, y_0, v, a, K_no_drag, g, m, '#00ff00');
+  draw(x_0, y_0, v, a, K, g, m, '#000000');
+}
 
-  while (i < 1000 && scale * last(ys) >= 0 && scale * last(xs) < coordinatesystem.width) {
+function last (arr) {
+  return arr[arr.length - 1];
+}
+
+var xs, ys;
+function draw (x0, y0, v_0, a_0, K, g, m, color) {
+  var scale = 100;
+  var delta_t = 0.01;
+  var i = 0;
+  ctx.strokeStyle = color || '#000000';
+
+  xs = [x0];
+  ys = [y0];
+
+  // copy, do not use reference
+  var v = Object.assign({}, v_0);
+  var a = Object.assign({}, a_0);
+
+  ctx.moveTo(scale * xs[0], -scale * ys[0]);
+  ctx.beginPath();
+
+  while (i < 1000
+         && scale * last(xs) < coordinatesystem.width
+         && last(ys) >= 0) {
     var x = last(xs) + v.x * delta_t;
     var y = last(ys) + v.y * delta_t;
 
@@ -74,15 +94,20 @@ function numericThrow () {
     v.y += a.y * delta_t;
 
     a.x = - K.x * Math.pow(v.x, 2) / m;
-    a.y = g - K.y * Math.pow(v.y, 2) / m;
+    a.y = g - Math.sign(v.y) * K.y * Math.pow(v.y, 2) / m;
+
+    if (i % 40 === 0) {
+      ctx.font = '16px arial';
+      var xx = x*scale;
+      var yy = -y*scale + (color === '#000000' ? 0 : -20);
+      ctx.strokeText(`a=[${a.x.toFixed(2)}, ${a.y.toFixed(2)}]  v=[${v.x.toFixed(2)},${v.y.toFixed(2)}]`, xx, yy);
+    }
 
     ctx.lineTo(scale * last(xs), -scale * last(ys));
     ctx.stroke();
 
     i += 1;
-  }
-}
 
-function last (arr) {
-  return arr[arr.length - 1];
+  }
+  ctx.closePath();
 }
